2525eabe124b8c1049a49c7781ce58fcc215994b
Dani <ddiaz@tactech.cl>
2022-07-04 18:26:58 -0400

    VALIDATE SOLUTIONS

--- Constants.java
+++ Constants.java
@@ -1,6 +1,7 @@
 public class Constants {
     public static final int POPULATION_SIZE = 25;
     public static final int EPOCH = 10;
+    public static final int OPTIMAL_VALUE = 101;
     public static final int MAX_ITTER = 1000;
     public static final double BETA = 1.5;
     public static final int N_VARS = 250;
--- Main.java
+++ Main.java
@@ -3,11 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		int qMetrics[], bestItters[] = new int[Constants.EPOCH];
-		int nVars, ps, q, suma;
-		double MaxFES;
+		int nVars, ps, suma, suma2;
+		double MaxFES, q;
 		Swarm swarm = null;
 		try {
 			suma = 0;
+			suma2 = 0;
 			nVars = Constants.N_VARS;
 			ps = Constants.POPULATION_SIZE;
 			MaxFES = (10000 * nVars)/(nVars*ps);
@@ -16,15 +17,21 @@ public class Main {
 				swarm = new Swarm();
 				swarm.execute();
 				bestItters[r-1] = swarm.getBestItter();
+				if (MaxFES >= bestItters[r-1]) {
+					suma+=1;
+					q = ((Constants.OPTIMAL_VALUE * 1.05) - 1)/((Constants.OPTIMAL_VALUE * 1.05) - 1) ;
+					System.out.println("BEST ITTER: "+bestItters[r-1]);
+				}
 				
 			}
 			for (int i = 0; i < bestItters.length; i++) {
 				if (MaxFES >= bestItters[i]){
-					suma+=1;
+					suma2+=1;
 				}
 				System.out.println("BEST ITTER: "+bestItters[i]);
 			}
 			System.out.println((suma/(Constants.EPOCH)));
+			System.out.println((suma2/(Constants.EPOCH)));
 		} catch (Exception e) {
 			StdOut.printf("%s\n%s", e.getMessage(), e.getCause());
 		}
