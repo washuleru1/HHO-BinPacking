52caca09557ee3313bda6bb014310ee7922648af
Dani <ddiaz@tactech.cl>
2022-07-04 16:32:19 -0400

    hho bin packing meta heuristic problem

---
+++ Hawk.java
@@ -0,0 +1,168 @@
+public class Hawk extends Problem {
+
+	private int[] x = new int[nVars];
+	private int[] p = new int[nVars];
+	private int S = 0;
+	private double v, r, u = 0;
+	private final int LB = 0;
+	private final int UB = 1;
+	private double rabbitJump = 0;
+	private double sigma = 0;
+	private double deltaXt = 0;
+	private double Y, levyFlight, Z = 0;
+	private double[] f = new double[nVars];
+	private double[] r0 = new double[nVars];
+	private double[] A = new double[nVars];
+
+	public Hawk() {
+		for (int j = 0; j < nVars; j++) {
+			x[j] = StdRandom.uniform(2);
+			p[j] = StdRandom.uniform(2);
+		}
+	}
+
+	public int[] getPositionVector() {
+		return x;
+	}
+
+	protected void exploration(Hawk g, Randoms randomValues, double[] averageHawksPosition) {
+		for (int j = 0; j < nVars; j++) {
+			if(randomValues.getQ() >= 0.5) {
+				//System.out.println("Q <= 0.5");
+				x[j]  =  toBinary(x[j] - StdRandom.uniform() * Math.abs(x[j] - (2 * StdRandom.uniform() * g.x[j])));
+			} else {
+				//System.out.println("Q > 0.5");
+				x[j] = toBinary(x[j] - averageHawksPosition[j] - randomValues.getR3() * (LB + randomValues.getR4() * (UB - LB)));
+			}
+		}
+	}
+
+	protected void softBeseige(Hawk g, double escapeEnergy) {
+		for (int j = 0; j < nVars; j++) {
+			deltaXt = g.p[j] - x[j];
+			rabbitJump = (2*(1 - StdRandom.uniform()));
+			x[j] = toBinary((deltaXt) - escapeEnergy * Math.abs(( rabbitJump * g.p[j]) - x[j]));
+		}	
+	}
+	
+	protected void hardBeseige(Hawk g, double escapeEnergy) {
+		for (int j = 0; j < nVars; j++) {
+			deltaXt = g.p[j] - x[j];
+			x[j] = toBinary(g.p[j] - escapeEnergy * Math.abs(deltaXt));
+		}
+	}
+
+	protected void softBeseigeProgresive(Hawk g, double escapeEnergy, double beta, double S, double u, double v) {
+		// la duda es si rabbitJump es en cada nvars o por cada iteracion
+		double gamma1 = gammaFunction(1 + beta);
+		double gamma2 = gammaFunction((1 + beta)/2);
+		sigma = Math.pow((gamma1 * Math.sin((Math.PI*beta)/2))/( gamma2 * beta * (2*(beta-1)/2)), (1/beta));
+		levyFlight = 0.01 * (u*sigma/Math.pow(Math.abs(v), (1/beta)));
+		for (int j = 0; j < nVars; j++) {
+			rabbitJump = (2*(1 - StdRandom.uniform()));
+			Y = g.p[j] - escapeEnergy * Math.abs((rabbitJump * g.p[j]) - x[j]);
+			Z = Y + S * levyFlight;
+			if (toBinary(Y) < toBinary(computeFitness(x))) {
+				x[j] = toBinary(Y);
+			} 
+			if (toBinary(Z) < toBinary(computeFitness(x))) {
+				x[j] = toBinary(Z);
+			}
+		}			
+	}
+
+	protected void hardBeseigeProgresive(Hawk g, double escapeEnergy, double beta, double[] averageHawksPosition, double S, double u, double v) {
+		double gamma1 = gammaFunction(1 + beta);
+		double gamma2 = gammaFunction((1 + beta)/2);
+		sigma = Math.pow((gamma1 * Math.sin((Math.PI*beta)/2))/( gamma2 * beta * (2*(beta-1)/2)), (1/beta));
+		levyFlight = 0.01 * (u*sigma/Math.pow(Math.abs(v), (1/beta)));
+		for (int j = 0; j < nVars; j++) {
+			rabbitJump = (2*(1 - StdRandom.uniform()));
+			Y = g.p[j] - escapeEnergy * Math.abs((rabbitJump * g.p[j]) - averageHawksPosition[j]);
+			Z = Y + S * levyFlight;
+			if (toBinary(Y) < toBinary(computeFitness(x))) {
+				x[j] = toBinary(Y);
+			} 
+			if (toBinary(Z) < toBinary(computeFitness(x))) {
+				x[j] = toBinary(Z);
+			}
+		}
+					
+	}
+
+	protected boolean isFeasible() {
+		return checkConstraint(x);
+	}
+
+	protected boolean isBetterThan(Hawk g) {
+		return computeFitness(x) < computeFitness(g.x);
+	}
+
+	private int toBinary(double x) {
+		return StdRandom.uniform() <= (1 / (1 + Math.pow(Math.E, -x))) ? 1 : 0;
+	}
+
+    protected double avgA() {
+    	return avg(A);
+    }
+    
+    private double avg(double[] a) {
+    	 return java.util.Arrays.stream(a).average().getAsDouble();
+    }
+    
+    protected void move(final Hawk g, final double fmin, final double fmax) {
+        double beta;
+        for (int j = 0; j < nVars; j++) {
+        }
+    }
+    private float diff() {
+		return computeFitness(x) - optimum();
+	}
+
+	private float rpd() {
+		return diff() / optimum() * 100f;
+	}
+
+	private String showSolution() {
+		return java.util.Arrays.toString(x);
+	}
+
+	protected void copy(Object object) {
+		if (object instanceof Hawk) {
+			System.arraycopy(((Hawk) object).x, 0, this.x, 0, nVars);
+			System.arraycopy(((Hawk) object).p, 0, this.p, 0, nVars);
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("optimal value: %d, fitness: %d, diff: %.1f, rpd: %.2f%%, p: %s", optimum(),
+				computeFitness(x), diff(), rpd(), showSolution());
+	}
+
+    public boolean isBetterThanPBest() {
+        return computeFitness() < computeFitnessPBest();
+    }
+	
+	protected int computeFitnessPBest() {
+		return computeFitness(p);
+	}
+
+	protected int computeFitness() {
+		return computeFitness(x);
+	}
+
+    protected void updatePBest() {
+		System.arraycopy(x, 0, p, 0, x.length);
+	}
+
+	static double logGamma(double x) {
+		double tmp = (x - 0.5) * Math.log(x + 4.5) - (x + 4.5);
+		double ser = 1.0 + 76.18009173    / (x + 0)   - 86.50532033    / (x + 1)
+						 + 24.01409822    / (x + 2)   -  1.231739516   / (x + 3)
+						 +  0.00120858003 / (x + 4)   -  0.00000536382 / (x + 5);
+		return tmp + Math.log(ser * Math.sqrt(2 * Math.PI));
+	}
+
+	static double gammaFunction(double x) { return Math.exp(logGamma(x)); }
+}
---
+++ Swarm.java
@@ -0,0 +1,136 @@
+
+public class Swarm {
+	
+	private final int ps = 25;
+	private final int T = 1000;
+	private double escapeEnergy = 0;
+    private Randoms randomValues;
+    private double Upper = 1;
+    private double Lower = -1;
+	private double baseEnergy = 0;
+	private double E1, r, S, u, v;
+	private static final double beta = 1.5;
+	
+	private java.util.List<Hawk> swarm = null;
+	private Hawk g = null;
+	
+	public void execute() {
+		init();
+		evolve();
+	}
+	
+	private void init() {
+		swarm = new java.util.ArrayList<Hawk>();
+		g = new Hawk();
+		Hawk hawk;
+		for(int i = 1; i <= ps; i++) {
+			do {
+				hawk = new Hawk();	
+			} while(!hawk.isFeasible());
+			swarm.add(hawk);
+		}
+		g.copy(swarm.get(0));
+		for (int i = 1; i < ps; i++) {
+			if (swarm.get(i).isBetterThan(g)) {
+				g.copy(swarm.get(i));
+			}
+		}
+		log(0);
+	}
+	
+	private void evolve() {
+		int t = 1;
+		Hawk hawk = new Hawk();
+		while(t <= T) {
+			//seteo de valores randomicos por iteración
+			
+			E1 = 2.0*(1 - (t/T));
+			//se recorre cada halcon del enjambre
+			for (int i = 0; i < ps; i++) {
+				//Se setea E0
+				baseEnergy = StdRandom.uniform(Lower, Upper);
+				//Se le da valor a escapeEnergy
+				escapeEnergy = baseEnergy * E1;
+				if (Math.abs(escapeEnergy) >= 1) {
+					//exploración
+					do {
+						//Se configuran valores randomicos y se elige un halcon aleatorio para la exploración
+						randomValues = new Randoms();
+						hawk.copy(swarm.get(StdRandom.uniform(ps)));
+						hawk.exploration(g, randomValues, averageHawksPosition());
+					} while (!hawk.isFeasible());
+					swarm.get(i).copy(hawk);
+				} else {
+					do{
+						//seteo de r randomico y halcon
+						r = StdRandom.uniform();
+						hawk.copy(swarm.get(i));
+						//soft beseige
+						if (r >= 0.5 && Math.abs(escapeEnergy) >= 0.5) {
+							//System.out.println("SOFT BESEIGE");
+							hawk.softBeseige(g, escapeEnergy);
+						}
+						//hard beseige
+						if (r >= 0.5 && Math.abs(escapeEnergy) < 0.5) {
+							//System.out.println("HARD BESEIGE");
+							hawk.hardBeseige(g, escapeEnergy);
+						}
+						//soft beseige with progresive rapid dives
+						if (r < 0.5 && Math.abs(escapeEnergy) >= 0.5) {
+							S = StdRandom.uniform(2);
+							u = StdRandom.uniform();
+							v = StdRandom.uniform();
+							//System.out.println("SOFT PROGRESIVE");
+							hawk.softBeseigeProgresive(g, escapeEnergy, beta, S, u, v);
+						}
+						//hard beseige with progresive rapid dives
+						if (r < 0.5 && Math.abs(escapeEnergy) < 0.5) {
+							S = StdRandom.uniform(2);
+							u = StdRandom.uniform();
+							v = StdRandom.uniform();
+							//System.out.println("HARD PROGRESIVE");
+							hawk.hardBeseigeProgresive(g, escapeEnergy, beta, averageHawksPosition(), S, u, v);
+						}
+					} while(!hawk.isFeasible());
+					swarm.get(i).copy(hawk);
+				}
+				//do {
+				//	hawk.copy(swarm.get(i));
+				//	//hawk.move(g, fmin, fmax);
+				//} while (!hawk.isFeasible());
+				//if (hawk.hasExploitation() && !hawk.isBetterThan(g)) {
+				//	//hawk.decreasesLoudness(alpha);
+				//	//hawk.increasesPulseEmission(gamma, t);
+				//}
+				if (hawk.isBetterThanPBest()) {
+					hawk.updatePBest();
+				}
+				if (hawk.isBetterThan(g)) {
+					g.copy(hawk);
+				}
+			}
+			log(t);
+			t++;
+		}
+	}
+	
+	private void log(int t) {
+		StdOut.printf("t=%d,\t%s\n", t, g);
+	}
+	
+	private double[] averageHawksPosition() {
+		int nVars = swarm.get(0).nVars;
+		double sum = 0;
+		double[] data = new double[nVars];
+		for (int i = 0; i < nVars; i++) {
+			for (int j = 0; j < ps; j++) {
+				sum+=swarm.get(j).getPositionVector()[i];
+				//System.out.println(sum);
+			}
+			data[i] = sum/ps;
+			sum=0;
+		}
+		return data;
+	}
+
+}
